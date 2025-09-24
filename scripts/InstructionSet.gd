a63c97c7d03e6e3d61d53a3e45aa8c732a588349
EmilienRY <emilien.rey1@gmail.com>
2025-09-24 09:14:03 +0200

    ajout base pour le terminak

---
+++ scripts/InstructionSet.gd
@@ -0,0 +1,25 @@
+class_name InstructionSet
+
+const NOT_FOUND = "not_found"
+
+const HELP = "help"
+
+const RESET = "reset"
+
+const QUIT = "quit"
+
+const LOOK = "look"
+
+const NORTH = "north"
+
+const SOUTH = "south"
+
+const EAST = "east"
+
+const WEST = "west"
+
+const GET = "get"
+
+const OPEN = "open"
+
+const CLOSE = "close"
---
+++ scripts/TextParser.gd
@@ -0,0 +1,65 @@
+class_name TextParser
+
+var InstructionSet = load("res://scripts/InstructionSet.gd")
+
+var object = null
+
+# Parse a given input string into an instruction.
+func parse(text):
+	match text:
+		'go north':
+			return InstructionSet.NORTH
+		'north':
+			return InstructionSet.NORTH
+		'go south':
+			return InstructionSet.SOUTH
+		'south':
+			return InstructionSet.SOUTH
+		'go east':
+			return InstructionSet.EAST
+		'east':
+			return InstructionSet.EAST
+		'go west':
+			return InstructionSet.WEST
+		'west':
+			return InstructionSet.WEST
+
+		'look':
+			return InstructionSet.LOOK
+		'help':
+			return InstructionSet.HELP
+		'help me':
+			return InstructionSet.HELP
+
+		'reset':
+			return InstructionSet.RESET
+		'quit':
+			return InstructionSet.QUIT
+		'exit':
+			return InstructionSet.QUIT
+
+	if text.begins_with('get '):
+		var regex = RegEx.new()
+		regex.compile("get\\s(?<object>.*(\\s.*)?)")
+		var results = regex.search(text)
+		object = results.get_string('object')
+		return InstructionSet.GET
+
+	if text.begins_with('open '):
+		var regex = RegEx.new()
+		regex.compile("open\\s(?<object>.*(\\s.*)?)")
+		var results = regex.search(text)
+		object = results.get_string('object')
+		return InstructionSet.OPEN
+
+	if text.begins_with('close '):
+		var regex = RegEx.new()
+		regex.compile("close\\s(?<object>.*(\\s.*)?)")
+		var results = regex.search(text)
+		object = results.get_string('object')
+		return InstructionSet.CLOSE
+
+	return InstructionSet.NOT_FOUND
+
+func get_object():
+	return object
