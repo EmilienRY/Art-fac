4865aff96676a0b53eff9a584c3b595bf4e534cc
PaulDeligne <paul.deligne@etu.umontpellier.fr>
2025-10-01 10:11:40 +0200

    ajout transition vue de la feuille

--- scene/zoomFeuille.gd
+++ scene/zoomFeuille.gd
@@ -2,4 +2,6 @@ extends Control
 
 func _gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
-		queue_free()
+		var tween = create_tween()
+		tween.tween_property(self,"position",Vector2(0, get_viewport_rect().size.y),0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+		tween.tween_callback(Callable(self, "queue_free")).set_delay(0.5)
